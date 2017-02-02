165fcb8d251bb900f15c1b35d00a972d2954d5fa
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-02-02 15:29:20 -0500

    Docker cleaning utility updated to clean dangling images.

--- bash/clean_docker.sh
+++ bash/clean_docker.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+# Remove dangling docker images or all docker containers
+# 2017-02-02
 
-docker ps -a
-docker rm $(docker ps -a | grep rabbitmq:management | awk '{print $1}')
+if [ -z "$1" ]; then
+    docker images
+    list_of_images=$(docker images --filter dangling=true -q)
+    if [ ! -z "$list_of_images" ]; then
+        docker rmi $list_of_images
+    fi
+elif [ "$1" == "mgmt" ]; then
+    docker ps -a
+    mgmt_container=$(docker ps -a | grep rabbitmq:management | awk '{print $1}')
+    if [ ! -z "$mgmt_container" ]; then
+        docker rm $mgmt_container
+    fi
+fi
 
